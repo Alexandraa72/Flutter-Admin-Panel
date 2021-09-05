ef53765b37712609ac15164dbfd543aefcb54c24
Alex <43963529+Alexandraa72@users.noreply.github.com>
2021-09-05 17:46:09 +0800

    update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+final Future<FirebaseApp> initialization = Firebase.initializeApp();
+FirebaseFirestore firebaseFireStore = FirebaseFirestore.instance;
+FirebaseAuth auth = FirebaseAuth.instance;
 
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
-
-
 const defaultPadding = 16.0;
